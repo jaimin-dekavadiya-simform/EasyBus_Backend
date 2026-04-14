0c749c7d7849739a13a6f29efb3568e2d3184277
jaimin-dekavadiya-simform <jaimin.dekavadiya@simformsolutions.com>
2026-04-14 17:57:34 +0530

    init:Project configuration Completed

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+    },
+  },
+
+  configPrettier,
+];
